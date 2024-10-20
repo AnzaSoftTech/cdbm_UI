601f7c8f76acd62784f00d84da6010096801ebfe
fahad <fahadhashmi948@gmail.com>
2024-10-21 00:19:49 +0530

    footer change

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Footer = () => {
-  console.log("Footer")
+  console.log("Footerh")
   return (
     <footer>
       <span></span>

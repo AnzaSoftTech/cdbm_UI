fd3d8a74cc294598d3c8f017e258f37b234b65ce
mohdfahad11 <achyutpratham77@gmail.com>
2024-10-25 15:29:35 +0530

    Add report pdf and improve ui sidebar and add tailwind

--- src/components/start.js
+++ src/components/start.js
@@ -1,17 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import './App.css'; // Import the CSS file for styles
+import './App.css';
 import MainPanel from './MainPanel';
 import { useNavigate } from 'react-router-dom';
-import { FaBars, FaAngleDown, FaAngleRight } from "react-icons/fa";
+import { FaAngleDown, FaAngleRight, FaHome, FaUpload, FaFileAlt, FaDollarSign, FaFileInvoiceDollar, FaBook } from "react-icons/fa";
 import { Link } from 'react-router-dom';
 
 const DashboardPage = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-  const [currentPage, setCurrentPage] = useState('page1');
-
-  const handleMenuItemClick = (page) => {
-    setCurrentPage(page);
-  };
+  const [currentPage] = useState('page1');
+  const [openMenus, setOpenMenus] = useState({});
 
   // Toggle sidebar visibility
   const toggleSidebar = () => {
@@ -19,13 +16,14 @@ const DashboardPage = () => {
   };
 
   // Handle clicks outside the sidebar
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   const handleClickOutside = (event) => {
     if (isSidebarOpen && !event.target.closest('.sidebar') && !event.target.closest('.togglebtn')) {
       setIsSidebarOpen(false);
     }
   };
 
-  const [openMenus, setOpenMenus] = useState({});
+//   const [openMenus, setOpenMenus] = useState({});
 
   const toggleMenu = (menuName) => {
     setOpenMenus({
@@ -40,7 +38,7 @@ const DashboardPage = () => {
     return () => {
       document.removeEventListener('mousedown', handleClickOutside);
     };
-  }, [isSidebarOpen]);
+  }, [handleClickOutside, isSidebarOpen]);
 
   const navigate = useNavigate();
 
@@ -60,64 +58,72 @@ const DashboardPage = () => {
         <div className='pt-2'>
            <h4>Sodhani Securities Ltd.</h4>
         </div>
-        <div className='pt-2'>
+        <div className='pt-1'>
              <button className="btn btn-outline-light" onClick={handleLogout}>Logout</button> &nbsp;
         </div>
       </div>
 
       {/* Sidebar */}
-      <div>
-        <div className={`sidebar ${isSidebarOpen ? 'open' : ''}`}>
-          <div className="menu">
+      <div className={`sidebar ${isSidebarOpen ? 'open' : ''}`}>
+        <div className="menu">
 
-            <div className="menu-item">
-              <div className="menu-title hamburger" onClick={() => toggleMenu("dashboard")}>
-                <Link to="/dashboard" className="nav-link active">Dashboard</Link>
-              </div>
+          {/* Dashboard */}
+          <div className="menu-item">
+            <div className="menu-title" onClick={() => toggleMenu("dashboard")}>
+              <FaHome style={{ fontSize: '23px' }} />
+              <Link to="/dashboard" className="nav-link active">Dashboard </Link>{<FaAngleRight />}
             </div>
-            <div className="menu-item hamburger">
-              <div className="menu-title" onClick={() => toggleMenu("uploads")}>
-                CDBM Uploads {openMenus.uploads ? <FaAngleDown /> : <FaAngleRight />}
-              </div>
-              {openMenus.uploads && (
-                <div className="submenu">
-                  <div className="submenu-item"><Link to="/dashboard/sauda_upload/su_mainapp" className="nav-link">Sauda Upload</Link></div>
-                  <div className="submenu-item"><Link to="/dashboard/main_file_upload/mf_mainapp" className="nav-link">Master File Upload</Link></div>
-                  <div className="submenu-item"><Link to="/dashboard/cash_net_position/cnp_mainapp" className="nav-link">Cash Net Position</Link></div>
-                  <div className="submenu-item"><Link to="/dashboard/sauda_report/sr_mainapp" className="nav-link">Sauda Report</Link></div>
-                </div>
-              )}
-            </div>
-            <div className="menu-item hamburger">
-              <div className="menu-title" onClick={() => toggleMenu("financial")}>
-                Finance {openMenus.financial ? <FaAngleDown /> : <FaAngleRight />}
-              </div>
-              {openMenus.financial && (
-                <div className="submenu">
-                  <div className="submenu-item"><Link to="/dashboard/Cash_Bank_Master/CashBankMasterApp " className="nav-link">Cash/Bank Master</Link></div>
-                  <div className="submenu-item"><Link to="/dashboard/payment_vouchar/Payment_voucharApp" className="nav-link"> Payment/Receipt Vouchers</Link></div>
-                  <div className="submenu-item"><Link to="/dashboard/journal_vouchar/JOURNALApp " className="nav-link">Journal Voucher</Link></div>
-                  <div className="submenu-item"><Link to="/dashboard/dr_cr_note/drcrnotesApp " className="nav-link">Debit/Credit Notes</Link></div>
-                  <div className="submenu-item"><Link to="/dashboard/Contra_Entry/contraenteryApp " className="nav-link"> Contra Entry</Link></div>
-                  <div className="submenu-item"><Link to="/dashboard/Bank_Reco/bankRecoApp " className="nav-link"> Bank Reconciliation</Link></div>
-                  <div className="submenu-item"><Link to="/dashboard/openingBalance/openingbalanceApp " className="nav-link">Opening Balance</Link></div>
+          </div>
 
-                </div>
-              )}
+          {/* CDBM Uploads */}
+          <div className="menu-item">
+            <div className="menu-title" onClick={() => toggleMenu("uploads")}>
+              <FaUpload style={{ fontSize: '23px' }} />
+              CDBM Uploads {openMenus.uploads ? <FaAngleDown /> : <FaAngleRight />}
             </div>
-            <div className="menu-item hamburger">
-              <div className="menu-title" onClick={() => toggleMenu("reports")}>
-                Reports {openMenus.reports ? <FaAngleDown /> : <FaAngleRight />}
+            {openMenus.uploads && (
+              <div className="submenu">
+                <div className="submenu-item"><Link to="/dashboard/sauda_upload/su_mainapp" className="nav-link"><FaFileAlt style={{ marginRight: '8px' }} />Sauda Upload</Link></div>
+                <div className="submenu-item"><Link to="/dashboard/main_file_upload/mf_mainapp" className="nav-link"><FaFileAlt style={{ marginRight: '8px' }} />Master File Upload</Link></div>
+                <div className="submenu-item"><Link to="/dashboard/cash_net_position/cnp_mainapp" className="nav-link"><FaFileAlt style={{ marginRight: '8px' }} />Cash Net Position</Link></div>
+                <div className="submenu-item"><Link to="/dashboard/sauda_report/sr_mainapp" className="nav-link"><FaFileAlt style={{ marginRight: '8px' }} />Sauda Report</Link></div>
               </div>
-              {openMenus.reports && (
-                <div className="submenu">
-                  <div className="submenu-item"><Link to="/dashboard/trial_balance/tb_mainapp" className="nav-link">Trial Balance</Link></div>
-                  <div className="submenu-item"><Link to="/dashboard/ledger/l_mainapp" className="nav-link">Ledger</Link></div>
-                  <div className="submenu-item"><Link to="/dashboard/pdfGenerate/pdfFile" className="nav-link">pdf</Link></div>
-                  {/* <div className="submenu-item" ><Link to="/dashboard/page2" className="nav-link">page 2</Link></div> */}
-                </div>
-              )}
+            )}
+          </div>
+
+          {/* Financial */}
+          <div className="menu-item">
+            <div className="menu-title" onClick={() => toggleMenu("financial")}>
+              <FaDollarSign style={{ fontSize: '23px' }} />
+              Finance {openMenus.financial ? <FaAngleDown /> : <FaAngleRight />}
             </div>
+            {openMenus.financial && (
+              <div className="submenu">
+                <div className="submenu-item"><Link to="/dashboard/Cash_Bank_Master/CashBankMasterApp" className="nav-link"><FaFileInvoiceDollar style={{ marginRight: '8px' }} />Cash/Bank Master</Link></div>
+                <div className="submenu-item"><Link to="/dashboard/payment_vouchar/Payment_voucharApp" className="nav-link"><FaFileInvoiceDollar style={{ marginRight: '8px' }} /> Payment/Receipt Vouchers</Link></div>
+                  <div className="submenu-item"><Link to="/dashboard/journal_vouchar/JOURNALApp " className="nav-link"><FaFileInvoiceDollar style={{ marginRight: '8px' }} />Journal Voucher</Link></div>
+                  <div className="submenu-item"><Link to="/dashboard/dr_cr_note/drcrnotesApp " className="nav-link"><FaFileInvoiceDollar style={{ marginRight: '8px' }} />Debit/Credit Notes</Link></div>
+                  <div className="submenu-item"><Link to="/dashboard/Contra_Entry/contraenteryApp " className="nav-link"><FaFileInvoiceDollar style={{ marginRight: '8px' }} /> Contra Entry</Link></div>
+                  <div className="submenu-item"><Link to="/dashboard/Bank_Reco/bankRecoApp " className="nav-link"><FaFileInvoiceDollar style={{ marginRight: '8px' }} /> Bank Reconciliation</Link></div>
+                  <div className="submenu-item"><Link to="/dashboard/openingBalance/openingbalanceApp " className="nav-link"><FaFileInvoiceDollar style={{ marginRight: '8px' }} />Opening Balance</Link></div>
+                {/* Additional financial submenu items here */}
+              </div>
+            )}
+          </div>
+
+          {/* Reports */}
+          <div className="menu-item">
+            <div className="menu-title" onClick={() => toggleMenu("reports")}>
+              <FaBook style={{ fontSize: '23px' }} />
+              Reports {openMenus.reports ? <FaAngleDown /> : <FaAngleRight />}
+            </div>
+            {openMenus.reports && (
+              <div className="submenu">
+                <div className="submenu-item"><Link to="/dashboard/trial_balance/tb_mainapp" className="nav-link"><FaFileAlt style={{ marginRight: '8px' }} />Trial Balance</Link></div>
+                <div className="submenu-item"><Link to="/dashboard/ledger/l_mainapp" className="nav-link"><FaFileAlt style={{ marginRight: '8px' }} />Ledger</Link></div>
+                <div className="submenu-item"><Link to="/dashboard/pdfGenerate/SettlementReport" className="nav-link"><FaFileAlt style={{ marginRight: '8px' }} />PDF</Link></div>
+                {/* <div className="submenu-item" ><Link to="/dashboard/page2" className="nav-link">page 2</Link></div> */}              </div>
+            )}
           </div>
         </div>
       </div>
--- src/pages/pdfGenerate/DataTable.js
+++ src/pages/pdfGenerate/DataTable.js
@@ -1,146 +1,12 @@
 import React, { useState } from 'react';
 import { useTable, useSortBy, useFilters, usePagination } from 'react-table';
 import { Table, Button, Container, Form, Row, Col } from 'react-bootstrap';
-import { PDFDownloadLink, Document, Page, Text, View, StyleSheet } from '@react-pdf/renderer';
+import { PDFDownloadLink } from '@react-pdf/renderer';
+import MyPDFDocument from './generatePDF';  // Import from the new file
 import './DataTable.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-// PDF styles
-const styles = StyleSheet.create({
-    page: {
-        padding: 13,
-        display: 'flex',
-        flexDirection: 'column',
-    },
-    header: {
-        fontSize: 7, // Font size for header text
-        marginBottom: 2, // Space between lines
-        textAlign: 'center', // Center text for the middle section
-    },
-    leftText: {
-        fontSize: 5, // Font size for left text
-        textAlign: 'left', // Align left text to the left
-        marginBottom: 2, // Space between lines for left text
-    },
-    righttext: {
-        fontSize: 5, // Font size for left text
-        textAlign: 'right', // Align left text to the left
-        marginBottom: 2, // Space between lines for left text
-    },
-    row: {
-        flexDirection: 'row', // Align items in a row
-        justifyContent: 'space-between', // Space between left and center content
-        alignItems: 'flex-start', // Align items at the top
-        width: '100%', // Full width
-        marginBottom: 10, // Space between this row and the next element
-    },
-    line: {
-        borderBottomWidth: 1, // Thickness of the line
-        borderBottomColor: 'black', // Line color
-        marginVertical: 2, // Space above and below the line
-        width: '100%', // Full width of the page
-    },
-});
-
-// Custom PDF document
-const MyPDFDocument = ({ tableData }) => (
-    <Document>
-        <Page style={styles.page} size="A4" orientation="landscape">  {/* Landscape mode */}
-
-            {/* Display left-side text and middle text at the top */}
-            <View fixed style={styles.row}>
-
-                {/* Left side text */}
-                <View style={{ width: '40%' }}>
-                    <Text style={styles.leftText}>Regd. Office: 304, Samarpan, New Link Road,</Text>
-                    <Text style={styles.leftText}>Chakala, Andheri East, Mumbai - 400 099.</Text>
-                    <Text style={styles.leftText}>Tel: 022-28316600, Fax: 022 2831 6605,</Text>
-                    <Text style={styles.leftText}>Email: sodhani.vt@gmail.com</Text>
-                    <Text style={styles.leftText}>Grievance Email: sodhani.relations@gmail.com</Text>
-                    <Text style={styles.leftText}>Director: Anil Sodhani / Anand Sodhani</Text>
-                    <Text style={styles.leftText}>Auth. Sign: Aditya Sodhani / Roopam Sodhani</Text>
-                </View>
-
-
-
-                {/* Middle text */}
-                <View style={{ width: '90%', alignItems: 'center' }}>
-                    <Text style={styles.header}>CONTRACT NOTE CUM TAX INVOICE</Text>
-                    <Text style={styles.header}>(TAX INVOICE UNDER SECTION 31 OF GST ACT)</Text>
-                    <Text style={{ fontSize: 11, textAlign: 'center' }}>SODHANI SECURITIES LTD</Text>
-                    <Text style={styles.header}>MEMBER: NATIONAL STOCK EXCHANGE OF INDIA LTD</Text>
-                    <Text style={styles.header}>SEBI REGN. NO. INZ000242534 • TRADING CODE NO: 23/10245 • CM BP ID: IN554382</Text>
-                    <Text style={styles.header}>CIN: U67120MH1997PLC108674 • GSTIN: 27AABCS • PAN NO AABCS9766K</Text>
-                    <Text style={styles.header}>Compliance Officer: Anil Sodhani • Phone: 022-2831 6600 • Email: sodhani.anil@gmail.com</Text>
-                </View>
-                {/* right  side text */}
-                <View style={{ width: '40%', alignItems: 'right' }}>
-                    <Text style={styles.righttext}>Revised / Supplementary</Text>
-                    <Text style={styles.righttext}>Original for Recipient / Duplicate for Supplier</Text>
-                    <Text style={styles.righttext}>Dealing Office Address:</Text>
-                    <Text style={styles.righttext}>304, Samarpan, New Link Road,</Text>
-                    <Text style={styles.righttext}>Chakala, Andheri East,</Text>
-                    <Text style={styles.righttext}>Mumbai - 400 099.</Text>
-                </View>
-            </View>
-
-            {/* Horizontal line */}
-            <View style={styles.line} />
-
-            {/* Left side text */}
-            <View style={{ width: '20%' }}>
-                <Text style={styles.leftText}>To,</Text>
-                <Text style={styles.leftText}>KISHORI GUPTA (HUF) (1K009)</Text>
-                <Text style={styles.leftText}>50 VAISHNO DEVI, 40 VITHAL NAGAR</Text>
-                <Text style={styles.leftText}>J V P D VILEPARLE W</Text>
-                <Text style={styles.leftText}>MUMBAI 400049</Text>
-            </View>
-            <View style={{ width: '20%' }}>
-                <Text style={styles.leftText}>Sir/Madam,</Text>
-                <Text style={styles.leftText}>we have this day done by your order and on ypur account the following transaction: </Text>
-            </View>
-
-
-            {/* bottom Table Data */}
-            <View style={{ width: '100%', position: 'absolute', bottom: 10, }} />
-
-            <View style={{ width: '70%', position: 'absolute', bottom: 10, left: 10 }}>
-                <Text style={styles.leftText}>OTHER LEVIES, IF ANY:</Text>
-                <Text style={styles.leftText}>Description of Service: Brokerage & related Securities & Commodity services. Accounting Code: 997152.
-                    SSL is collecting Stamp Duty & Securities Transaction Tax (STT) as a pure agent of the investor and hence the same is not considered in taxable value of supply for charging GST.
-                    Transactions mentioned in this contract note cum bill shall be governed and subject to the Rules, Bye-laws, Regulations and Circulars of the respective Exchanges on which trades have been executed and Securities and Exchange Board of India issued from time to time. It shall also be subject to the relevant Acts, Rules, Regulations, Directives, Notifications, Guidelines (including GST Laws) & Circulars issued by SEBI / Government of India / State Governments
-                    and Union Territory Governments issued from time to time. The Exchanges provide Complaint Resolution, Arbitration and Appellate arbitration facilities at the Regional Arbitration Centres (RAC). The client may approach its nearest centre, details of which are available on respective Exchange's website. Please visit www.nseindia.com for NSE, www.bseindia.com for BSE and www.msei.in for MSEI.
-                    Date: 18 Oct, 2024. Place: Mumbai
-                </Text>
-
-            </View>
-
-
-            <View style={{ width: '70%', position: 'absolute', bottom: 60, right: 10 }}>
-                <Text style={styles.righttext}>Your Faithfully,for Sodhani Securities Ltd.:</Text>
-            </View>
-            <View style={{ width: '70%', position: 'absolute', bottom: 7, right: 10 }}>
-                <Text style={styles.righttext}>Director/Authorised Signatory </Text>
-                <Text style={styles.righttext}>Member: National Stock Exchange of India Ltd</Text>
-            </View>
-
-
-
-            {/* Table Data */}
-            {tableData.map((row, index) => (
-                <View key={index} style={{ flexDirection: 'row', width: '100%', marginBottom: 3 }}>
-                    {Object.values(row).map((cell, cellIndex) => (
-                        <Text key={cellIndex} style={{ marginRight: 5 }}>{cell}</Text>
-                    ))}
-                </View>
-            ))}
-
-        </Page>
-    </Document>
-);
-
 const DataTable = ({ columns, data }) => {
-    const [reportType, setReportType] = useState('partwise');
     const [accountName, setAccountName] = useState('');
     const [showTable, setShowTable] = useState(false);
 
@@ -151,50 +17,30 @@ const DataTable = ({ columns, data }) => {
         page,
         prepareRow,
     } = useTable(
-        {
-            columns,
-            data,
-            initialState: { pageIndex: 0 },
-        },
+        { columns, data, initialState: { pageIndex: 0 } },
         useFilters,
         useSortBy,
         usePagination
     );
 
-    const handleRunReport = () => {
-        setShowTable(true);
-    };
+    const handleRunReport = () => setShowTable(true);
 
     return (
         <Container className="align-items-center">
-            <Row className="mb-3">
-                <Col xs={12} md={6} className="d-flex align-items-center label-color-common">
-                    <Form.Check
-                        type="radio"
-                        label="Party"
-                        name="reportType"
-                        value="part"
-                        checked={reportType === 'part'}
-                        onChange={(e) => setReportType(e.target.value)}
-                        className="mr-3"
-                        defaultChecked
-                    />
-                    <Form.Check
-                        type="radio"
-                        label="General"
-                        name="reportType"
-                        value="general"
-                        style={{ marginLeft: '5%' }}
-                        checked={reportType === 'general'}
-                        onChange={(e) => setReportType(e.target.value)}
-                    />
-                </Col>
+            <Row className="pt-5 mb-3">
                 <Col xs={12} md={6} className="d-flex align-items-center">
-                    <Form.Label className="margin-right mb-0 label-color-common" style={{ width: "25%" }}>Account Name </Form.Label>
+                    <Form.Label className="margin-right mb-0 label-color-common" style={{ width: "25%" }}>Settlement No:</Form.Label>
                     <Form.Control
                         type="text"
                         value={accountName}
                         onChange={(e) => setAccountName(e.target.value)}
+                        size="sm"
+                    />
+                </Col>
+                <Col xs={12} md={6} className="d-flex align-items-center">
+                    <Form.Label className="margin-right mb-0 label-color-common" style={{ width: "25%" }}>Select Date: </Form.Label>
+                    <Form.Control
+                        type="date"
                         size="sm"
                     />
                 </Col>
@@ -206,9 +52,8 @@ const DataTable = ({ columns, data }) => {
                         Run Report
                     </Button>
 
-                    {/* PDF Download Button */}
                     {showTable && (
-                        <PDFDownloadLink
+                        <PDFDownloadLink 
                             document={<MyPDFDocument tableData={page.map(row => row.values)} />}
                             fileName="Ledger.pdf"
                             style={{ textDecoration: 'none' }}
@@ -230,10 +75,10 @@ const DataTable = ({ columns, data }) => {
                             {headerGroups.map(headerGroup => (
                                 <tr {...headerGroup.getHeaderGroupProps()}>
                                     {headerGroup.headers.map(column => (
-                                        <th {...column.getHeaderProps(column.getSortByToggleProps())} style={{ width: column.id === 'particular' ? '400px' : 'auto' }} className="custom-header">
+                                        <th {...column.getHeaderProps(column.getSortByToggleProps())} style={{ width: column.id === "no" ? "100px" : "auto" }}>
                                             {column.render('Header')}
                                             <span>
-                                                {column.isSorted ? (column.isSortedDesc ? ' ↓' : ' ↑') : ''}
+                                                {column.isSorted ? (column.isSortedDesc ? ' 🔽' : ' 🔼') : ''}
                                             </span>
                                         </th>
                                     ))}
@@ -255,8 +100,6 @@ const DataTable = ({ columns, data }) => {
                     </Table>
                 </div>
             )}
-
-            {/* Pagination and other controls (if needed) */}
         </Container>
     );
 };

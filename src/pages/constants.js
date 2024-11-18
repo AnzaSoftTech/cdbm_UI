db6d3dc3312efd040dc91ce91f0e6fbef441955d
AnzaSoftTech <91406440+AnzaSoftTech@users.noreply.github.com>
2024-11-18 14:51:48 +0530

    Update constants.js

--- src/pages/constants.js
+++ src/pages/constants.js
@@ -1,5 +1,5 @@
 // constants.js
-export const BASE_URL = "http://13.233.84.104:3001"
+export const BASE_URL = "http://13.233.84.104:3001";
 //  process.env.NODE_ENV === "production"
 //    ? process.env.API_URL
 //    : "http://localhost:3001";

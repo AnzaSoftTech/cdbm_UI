76e248e2149dc1ac8e65b62896ce9e857b31b4f8
anza.soft.tech@gmail.com <anza.soft.tech@gmail.com>
2024-12-10 12:12:28 +0530

    more changes

--- src/pages/constants.js
+++ src/pages/constants.js
@@ -1,8 +1,8 @@
 // constants.js
 
-// export const BASE_URL = "http://13.233.84.104:3001";
+export const BASE_URL = "http://13.233.84.104:3001";
 
-export const BASE_URL = "http://localhost:3001";
+// export const BASE_URL = "http://localhost:3001";
 
 // export const BASE_URL =
 //   process.env.NODE_ENV === "production"
